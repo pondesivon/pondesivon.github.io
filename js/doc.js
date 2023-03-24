11fd3cd8dfcfd50d7887616c4a73f9e610cd1ab5
Mahir <126827264+pondesivon@users.noreply.github.com>
2023-03-25 02:13:55 +0300

    toplu tasarım değişikliği ve içerik ekleme

--- js/doc.js
+++ js/doc.js
@@ -1,8 +1,35 @@
+//--------------------------------------------------
+//li Elementleri İçin Filtreleme Yap
+//--------------------------------------------------
 //https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
-function konuFiltre() {
+function konuFiltre(idDeger) {
   // Declare variables
   var input, filter, table, li, td, i, txtValue;
-  input = document.getElementById("konu");
+  input = document.getElementById(idDeger);
+  filter = input.value.toLocaleLowerCase('tr-TR');
+  liste = document.getElementById("liste");
+  li = liste.getElementsByTagName("li");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a) {
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toLocaleLowerCase('tr-TR').indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+
+    input.select();
+  }
+}
+
+function konuFiltreKey(idDeger) {
+  // Declare variables
+  var input, filter, table, li, td, i, txtValue;
+  input = document.getElementById(idDeger);
   filter = input.value.toLocaleLowerCase('tr-TR');
   liste = document.getElementById("liste");
   li = liste.getElementsByTagName("li");
@@ -25,6 +52,9 @@ function konuFiltre() {
   }
 }
 
+//--------------------------------------------------
+//Bütün İçerikleri XML Üzerinden Listele
+//--------------------------------------------------
 function xmlDosyaYukle() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
@@ -65,4 +95,61 @@ function icerikListe(xml) {
     // Print the xml data in table form
     document.getElementById("icerik-liste").innerHTML = liste;
     //y.select();
+}
+
+
+//--------------------------------------------------
+//Son N İçeriği XML Üzerinden Listele
+//--------------------------------------------------
+function xmlDosyaYukle(sayi) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+
+        // Request finished and response 
+        // is ready and Status is "OK"
+        if (this.readyState == 4 && this.status == 200) {
+            icerikListe(this, sayi);
+        }
+    };
+
+    // employee.xml is the external xml file
+    xmlhttp.open("GET", "http://localhost/pondesivon.github.io/icerikler.xml", true);
+    xmlhttp.send();
+}
+
+function icerikListe(xml, sayi) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var liste = "<ol id='liste'>";
+    var x = xmlDoc.getElementsByTagName("Icerik");
+    // Start to fetch the data by using TagName 
+    for (i = x.length - 1 ; i >= (x.length - sayi); i--) {
+        liste += '<li class="str">'
+              + '<a class="bgl" target="_blank" href="' + x[i].getAttribute("baglanti") + '">'
+              + x[i].getAttribute("baslik")
+              + "</a></li>";
+    }
+
+    liste+= "</ol>";
+
+
+    // Print the xml data in table form
+    document.getElementById("icerik-liste").innerHTML = liste;
+    //y.select();
+}
+
+//--------------------------------------------------
+//input Elementi İçin Yazılmış Odaklanma Fonksiyonu
+//--------------------------------------------------
+function aramaKutusunaOdaklan(idAdi) {
+    var degisken = document.getElementById(idAdi);
+    degisken.select();
+}
+
+//--------------------------------------------------
+//input Elementi İçin Yazılmış Temizleme Fonksiyonu
+//--------------------------------------------------
+function valueTemizle(idAdi) {
+    var degisken = document.getElementById(idAdi);
+    degisken.value="";
 }
